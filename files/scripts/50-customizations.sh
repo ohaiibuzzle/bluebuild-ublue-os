9aaf8f7d771207e1d426687eb1ae7c0869319ce3
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-01-17 16:03:20 +0700

    added: initial files

---
+++ files/scripts/50-customizations.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+rpm-ostree override replace \
+    --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+    fwupd \
+    fwupd-plugin-flashrom \
+    fwupd-plugin-modem-manager \
+    fwupd-plugin-uefi-capsule-data
+
+# Switcheroo patch
+rpm-ostree override replace \
+    --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:sentry:switcheroo-control_discrete \
+    switcheroo-control
+
+rm /etc/yum.repos.d/_copr_sentry-switcheroo-control_discrete.repo
+
+# Starship Shell Prompt
+curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+# shellcheck disable=SC2016
+echo 'eval "$(starship init bash)"' >>/etc/bashrc
+
+# Bash Prexec
+curl --retry 3 -Lo /usr/share/bash-prexec https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh
+
+# Topgrade Install
+pip install --prefix=/usr topgrade
+
+# Register Fonts
+fc-cache -f /usr/share/fonts/ubuntu
+fc-cache -f /usr/share/fonts/inter
+
+# Configure Greetd to use tuigreet
+cat >/etc/greetd/config.toml <<EOF
+[terminal]
+vt = 1
+
+# The default session, also known as the greeter.
+[default_session]
+command = "tuigreet --cmd /bin/Hyprland"
+user = "greeter"
+EOF
+
+systemctl enable nvidia-{suspend,resume,hibernate} greetd
+
+# Enable greetd
+systemctl enable greetd
