e1164eb38d9b9d42aacf94c7b844ff7c7c2e95d6
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-03-26 16:20:54 +0700

    fix: move more fragile configs to root overrides instead

--- files/scripts/52-buzzle-dotfiles.sh
+++ files/scripts/52-buzzle-dotfiles.sh
@@ -4,19 +4,6 @@ set -eoux pipefail
 
 cd /tmp
 
-git clone https://github.com/ohaiibuzzle/dotfiles.git dotfiles
-
-# skel configs
-mkdir -p /etc/skel/.config/{hypr,wofi}
-mkdir -p /etc/xdg/waybar
-mkdir -p /usr/share/fonts/MiSans
-
-# copy configs over
-cp dotfiles/hypr/hypr{land,lock,idle}.conf /etc/skel/.config/hypr/
-cp -rf dotfiles/waybar/* /etc/xdg/waybar/
-cp -f dotfiles/wofi/* /etc/skel/.config/wofi/
-rm -rf dotfiles
-
 # Xiaomi MiSans typeface
 mkdir misans && cd misans
 curl -L -o 'MiSans_ALL.zip' 'https://hyperos.mi.com/font-download/MiSans_Global_ALL.zip'
@@ -41,9 +28,4 @@ mkdir sddm && cd sddm
 curl -L -o catppuccin.zip https://github.com/catppuccin/sddm/releases/download/v1.0.0/catppuccin-mocha.zip
 unzip catppuccin.zip
 mv catppuccin-mocha /usr/share/sddm/themes
-cat > /etc/sddm.conf << EOF
-[Theme]
-Current=catppuccin-mocha
-Font=MiSans
-EOF
 cd /tmp && rm -rf sddm
