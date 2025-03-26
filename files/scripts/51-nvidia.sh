c96d5b06532b86af7d6a30c362c1119e8064f33f
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-03-27 00:09:45 +0700

    feat: the way it's meant to be installed

---
+++ files/scripts/51-nvidia.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Enable RPMFusion
+dnf install -y \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install -y \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install akmod-nvidia
