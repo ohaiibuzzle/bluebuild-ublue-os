1928d425106055630600c31f141dc96781bb4b1e
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-03-26 16:11:20 +0700

    fix: use sddm.conf

--- files/scripts/52-buzzle-dotfiles.sh
+++ files/scripts/52-buzzle-dotfiles.sh
@@ -41,7 +41,7 @@ mkdir sddm && cd sddm
 curl -L -o catppuccin.zip https://github.com/catppuccin/sddm/releases/download/v1.0.0/catppuccin-mocha.zip
 unzip catppuccin.zip
 mv catppuccin-mocha /usr/share/sddm/themes
-cat > /etc/sddm.conf.d/theme.conf << EOF
+cat > /etc/sddm.conf << EOF
 [Theme]
 Current=catppuccin-mocha
 Font=MiSans
