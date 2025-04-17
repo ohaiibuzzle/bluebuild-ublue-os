fe14472d7c299d4430cd18f7b8d086e71645da59
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-04-18 00:08:22 +0700

    fix: steal akmods nvidia script from ublue

--- files/scripts/40-nvidia.sh
+++ files/scripts/40-nvidia.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-rpm-ostree install \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+mkdir /tmp/akmods-rpms
+skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-nvidia:main-"$(rpm -E %fedora)" dir:/tmp/akmods-rpms
+NVIDIA_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods-rpms/manifest.json | cut -d : -f 2)
+tar -xvzf /tmp/akmods-rpms/"$NVIDIA_TARGZ" -C /tmp/
+mv /tmp/rpms/* /tmp/akmods-rpms/
 
-rpm-ostree install akmod-nvidia
+# Install Nvidia RPMs
+curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh # Change when nvidia-install.sh updates
+chmod +x /tmp/nvidia-install.sh
+IMAGE_NAME="main" RPMFUSION_MIRROR="" /tmp/nvidia-install.sh
+rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
+ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
+rm -rf /tmp/akmods-rpms
