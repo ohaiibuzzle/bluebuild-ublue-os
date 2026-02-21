e63873625f593eb35420fa12308aa8af23a33123
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2026-02-21 17:26:07 +0700

    fix: nvidia image

--- files/scripts/40-nvidia.sh
+++ files/scripts/40-nvidia.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-mkdir /tmp/akmods-rpms
-skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-nvidia:main-"$(rpm -E %fedora)" dir:/tmp/akmods-rpms
+mkdir /tmp/akmods-nvidia /tmp/akmods-extract
+skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-nvidia:main-"$(rpm -E %fedora)" dir:/tmp/akmods-nvidia
 NVIDIA_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods-rpms/manifest.json | cut -d : -f 2)
-tar -xvzf /tmp/akmods-rpms/"$NVIDIA_TARGZ" -C /tmp/
-mv /tmp/rpms/* /tmp/akmods-rpms/
+tar -xvzf /tmp/akmods-nvidia/"$NVIDIA_TARGZ" -C /tmp/akmods-extract
 
-# Install Nvidia RPMs
-curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh # Change when nvidia-install.sh updates
-chmod +x /tmp/nvidia-install.sh
-IMAGE_NAME="main" RPMFUSION_MIRROR="" /tmp/nvidia-install.sh
-rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
-ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
-rm -rf /tmp/akmods-rpms
+dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+dnf5 -y install /tmp/akmods-extract/kernel-rpms/kernel*.rpm
+dnf5 -y install /tmp/akmods-extract/rpms/ublue-os/ublue-os-nvidia*.rpm
+dnf5 -y install /tmp/akmods-extract/rpms/kmods/kmod-nvidia*.rpm
+rm -rf /tmp/akmods-nvidia /tmp/akmods-extract
