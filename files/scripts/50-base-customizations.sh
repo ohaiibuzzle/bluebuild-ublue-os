3e017cc59af6e6da6c80edd302046baadd075525
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2026-02-21 19:40:08 +0700

    fix: use fwupd from fedora itself

--- files/scripts/50-base-customizations.sh
+++ files/scripts/50-base-customizations.sh
@@ -2,14 +2,6 @@
 
 set -eoux pipefail
 
-rpm-ostree override replace \
-    --experimental \
-    --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-    fwupd \
-    fwupd-plugin-flashrom \
-    fwupd-plugin-modem-manager \
-    fwupd-plugin-uefi-capsule-data
-
 # Starship Shell Prompt
 curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
 tar -xzf /tmp/starship.tar.gz -C /tmp
